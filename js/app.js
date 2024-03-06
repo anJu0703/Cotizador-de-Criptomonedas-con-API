4bc1813c2ca2cd9f2f8bb3b25d72f9863a2ef636
anJu0703 <jpmantilla2000@hotmail.com>
2024-03-06 13:02:28 -0500

    Insertando criptos a Select

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//Variables del documento//
+
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+
+//Crear promise//
+
+const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas);
+});
+
+//addEventListener//
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    consultarCriptomonedas();
+});
+
+
+//Funciones//
+
+function consultarCriptomonedas() {
+
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(resultado => obtenerCriptomonedas(resultado.Data))
+    .then(criptomonedas => selectCriptomonedas(criptomonedas))
+};
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
+        const {FullName, Name} = cripto.CoinInfo;
+
+        const option = document.createElement('option');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option);
+    });
+}
